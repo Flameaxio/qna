d3d339679ce6c268f40a23be88b5b5ff5d3cbe6b
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-03-22 14:04:52 +0200

    Removed index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_132146) do
+ActiveRecord::Schema.define(version: 2021_03_22_115609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,10 +134,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_132146) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "confirmation_token"
     t.string "unconfirmed_email"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
